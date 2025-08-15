20061a0f476102ecc01123352189fd16bf2ed09a
pavel-2009 <ms.radionova@bk.ru>
2025-08-15 14:05:16 +0500

    Создано приложение

--- anfisa/anfisa/settings.py
+++ anfisa/anfisa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'ice_cream.apps.IceCreamConfig',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
