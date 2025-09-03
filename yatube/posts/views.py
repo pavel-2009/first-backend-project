a414d82c666ece13882a90130ae2f671a42a4352
pavel-2009 <ms.radionova@bk.ru>
2025-09-03 12:54:12 +0500

    ааа

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,8 +3,21 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'posts/index.html')
+    text = 'Это главная страница проекта Yatube.'
+    context = {
+        'text': text,
+    }
+
+    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request):
-    return render(request, 'posts/group_list.hrml')
+def group_posts(request, slug):
+    slug = slug
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'slug': slug,
+        'text': text,
+    }
+
+    return render(request, 'posts/group_list.html', context)
+
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from posts import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
+    path('', include('posts.urls', namespace='posts'))
 ]
