29fb77b392b2a5e2fb41ee4ceb688f511a415b6e
pavel-2009 <ms.radionova@bk.ru>
2025-09-08 14:59:12 +0500

    Добавлена страница выхода

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts'))
+    path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('users.urls', namespace='users')),
 ]
