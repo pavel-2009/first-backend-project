9cc5aff229adf1311a820ef59f40cf415a6fce9b
pavel-2009 <ms.radionova@bk.ru>
2025-12-05 17:38:46 +0500

    d

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # потому что static лежит в приложении posts
