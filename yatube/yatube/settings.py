fb211e96ea44d0c2306197f377b58fc1cc70ee3f
pavel-2009 <ms.radionova@bk.ru>
2025-12-06 12:27:53 +0500

    d

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fepfor+05@hno%jjc7*t4fr#jwxy@ef=)r)^mkv@@p##z((1i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
